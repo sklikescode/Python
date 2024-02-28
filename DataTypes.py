007553aa4b09cef7f99fb4f3d91957d0d4219435
sklikescode <karikonyistephen@gmail.com>
2024-02-28 14:34:56 +0300

    new file on python data types

---
+++ DataTypes.py
@@ -0,0 +1,52 @@
+# <class 'int'>
+a=100
+print("The data type of", a, "is", type(a))
+print(a)
+
+# <class 'bool'>
+b=True
+print("The data type of", b, "is", type(b))
+print(b)
+
+# <class 'float'>
+c=20.345
+print("The data type of", c, "is", type(c))
+print(c)
+
+# <class 'complex'>
+d=10+3j
+print("The data type of", d, "is", type(d))
+
+# String Data Types
+str = 'Hello World'
+print(str)
+print(str[0])
+print(str[2:5])
+print(str[5:10])
+print(str[5:])
+print(str + " Class")
+print(str *4)
+
+#List Data Types
+list = ['abcd', 756, 43.2, 'John']
+tinylist = ['abcd', 'JOhn']
+print(list)
+print(list[0])
+print(tinylist)
+print(list[2:3])
+print(list + tinylist)
+print(tinylist *3)
+
+
+# Tuples
+tuple = (1, 2, 3, 'John', 40.3, 7+3j)
+print(tuple)
+print(tuple[2:3])
+print(tuple[2:])
+print(tuple *2)
+
+# Dictionary
+dict = 
+
+
+
